266e548382e3119ea96ed50c78a0fb3397f0fb1f
Teleejr <81259920+Teleejr@users.noreply.github.com>
2021-06-10 14:35:38 -0400

    Added services functionality

--- prepour/prepour/src/main/java/org/perscholas/services/CustomerService.java
+++ prepour/prepour/src/main/java/org/perscholas/services/CustomerService.java
@@ -1,8 +1,42 @@
 package org.perscholas.services;
 
+import org.perscholas.dao.ICustomerRepo;
+import org.perscholas.models.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service @Transactional
 public class CustomerService {
+
+    //Use the Customer repository
+    ICustomerRepo customerRepo;
+
+    //Create a constructor
+    @Autowired
+    public CustomerService(ICustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
+    }
+
+    //Find all customers
+    public List<Customer> findAllCustomers() {
+        return customerRepo.findAll();
+    }
+
+    //Get a customer by id
+    public Customer getCustomerById(Long id) {
+        return customerRepo.getById(id);
+    }
+
+    //Delete a customer by id
+    public void deleteCustomerById(Long id) {
+        customerRepo.deleteById(id);
+    }
+
+    //Save a customer
+    public Customer saveCustomer(Customer customer) {
+        return customerRepo.save(customer);
+    }
+
 }
--- prepour/prepour/src/main/java/org/perscholas/services/TabService.java
+++ prepour/prepour/src/main/java/org/perscholas/services/TabService.java
@@ -1,8 +1,41 @@
 package org.perscholas.services;
 
+import org.perscholas.dao.ITabsRepo;
+import org.perscholas.models.Tabs;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service @Transactional
 public class TabService {
+
+    //Use Tab repository
+    ITabsRepo tabsRepo;
+
+    //Create a constructor
+    @Autowired
+    public TabService(ITabsRepo tabsRepo) {
+        this.tabsRepo = tabsRepo;
+    }
+
+    //Find all Tabs
+    public List<Tabs> findAllTabs() {
+        return tabsRepo.findAll();
+    }
+
+    //Get Tabs by Id
+    public Tabs getTabsById(Long id) {
+        return tabsRepo.getById(id);
+    }
+
+    //Delete a Tab
+    public void deleteTab(Long id) {
+        tabsRepo.deleteById(id);
+    }
+
+    //Save Tab
+    public Tabs saveTab(Tabs tab) {
+        return tabsRepo.save(tab);
+    }
 }
