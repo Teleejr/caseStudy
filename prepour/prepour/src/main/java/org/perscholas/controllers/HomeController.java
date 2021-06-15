1b65a23cfec5c9c69a6633e3d6af0c989657e1a0
Teleejr <81259920+Teleejr@users.noreply.github.com>
2021-06-15 16:33:25 -0400

    Honestly? I don't remember what I did here.

--- prepour/prepour/src/main/java/org/perscholas/controllers/HomeController.java
+++ prepour/prepour/src/main/java/org/perscholas/controllers/HomeController.java
@@ -1,12 +1,30 @@
 package org.perscholas.controllers;
 
+import lombok.extern.java.Log;
+import org.perscholas.models.Tabs;
+import org.perscholas.services.TabService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
+
+@Log
 @Controller
 @RequestMapping("app")
 public class HomeController {
+
+    TabService tabService;
+
+    @Autowired
+    public HomeController(TabService tabService) {
+        this.tabService = tabService;
+    }
 
     @GetMapping("/home")
     public String home() {
@@ -27,4 +45,30 @@ public class HomeController {
     public String menu() {
         return "menu";
     }
+
+    //Tab page
+    @ModelAttribute("tab")
+    public Tabs initTabs() {
+        return new Tabs();
+    }
+
+    @PostMapping("/createtab")
+    public String newTab(@ModelAttribute("tab") @Valid Tabs tab, BindingResult result, Model model) {
+
+        System.out.println(result.hasErrors());
+        if(result.hasErrors()) {
+            log.warning("In if statement");
+            log.warning("Error count: " + String.valueOf(result.getErrorCount()));
+            return "index";
+        }
+        else {
+            log.warning("NOT in if statement");
+            log.info("Tab: " + tab);
+            Tabs dbTabs = tabService.saveTab(tab);
+            model.addAttribute("tab", tab);
+            return "menu";
+        }
+    }
+    //Register page
+    //Connect menu to db
 }
