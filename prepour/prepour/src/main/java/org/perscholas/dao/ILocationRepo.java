e8cc282786bb83c1918b0002931c16e090fab59e
Teleejr <81259920+Teleejr@users.noreply.github.com>
2021-06-18 13:41:43 -0400

    Changed 'Location' to 'Locations' in ILocationRepo

--- prepour/prepour/src/main/java/org/perscholas/dao/ILocationRepo.java
+++ prepour/prepour/src/main/java/org/perscholas/dao/ILocationRepo.java
@@ -1,10 +1,10 @@
 package org.perscholas.dao;
 
+import org.perscholas.models.Locations;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.xml.stream.Location;
 
 @Repository
-public interface ILocationRepo extends JpaRepository<Location, Long> {
+public interface ILocationRepo extends JpaRepository<Locations, Long> {
 }
